6214b8b948ea95967a6188639ad698b9e12e8d02
Hasnathasan <hasnatoooooooo@gmail.com>
2024-02-01 00:18:51 +0600

    Changes in useOrder.jsx file

--- src/Hooks/useOrders.jsx
+++ src/Hooks/useOrders.jsx
@@ -1,18 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-
 const useOrders = () => {
-    const { data: orders, isLoading: isOrdersLoading } = useQuery({
-        queryKey: ["orders"],
-        queryFn: async () => {
-            return axios.get('https://e-commerce-server-side-eosin.vercel.app/orders')
-            .then(result => {
-                return result.data;
-            });
-        },
-      });
-      return [orders, isOrdersLoading]
+  const { data: orders, isLoading: isOrdersLoading } = useQuery({
+    queryKey: ["orders"],
+    queryFn: async () => {
+      return axios
+        .get("https://e-commerce-server-side-eosin.vercel.app/orders")
+        .then((result) => {
+          return result.data;
+        });
+    },
+  });
+  return [orders, isOrdersLoading];
 };
 
 export default useOrders;
