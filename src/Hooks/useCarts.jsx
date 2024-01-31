9b3c7c4ee484b0ef30f9f9598e2cd0ca0e6db266
Hasnathasan <hasnatoooooooo@gmail.com>
2024-02-01 00:12:22 +0600

    UseCarts.jsx hook updated

--- src/Hooks/useCarts.jsx
+++ src/Hooks/useCarts.jsx
@@ -3,19 +3,25 @@ import axios from "axios";
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
 
-
 const useCarts = () => {
-    const {user} = useContext(AuthContext);
-    const { data: carts, isLoading: isCartsLoading, refetch } = useQuery({
-        queryKey: ['carts', user?.email],
-        queryFn: async() => {
-            return axios.get(`https://e-commerce-server-side-eosin.vercel.app/carts?email=${user?.email}`)
-                .then(result => {
-                    return result.data;
-                })
-        },
-      })
-      return [carts, isCartsLoading, refetch]
+  const { user } = useContext(AuthContext);
+  const {
+    data: carts,
+    isLoading: isCartsLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["carts", user?.email],
+    queryFn: async () => {
+      return axios
+        .get(
+          `https://e-commerce-server-side-eosin.vercel.app/carts?email=${user?.email}`
+        )
+        .then((result) => {
+          return result.data;
+        });
+    },
+  });
+  return [carts, isCartsLoading, refetch];
 };
 
 export default useCarts;
