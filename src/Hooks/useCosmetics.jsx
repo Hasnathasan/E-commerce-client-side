78c9224c065e9d46c2027689bd4afa1c0a3df689
Hasnathasan <hasnatoooooooo@gmail.com>
2024-02-01 00:16:59 +0600

    useCosmetics hook updated

--- src/Hooks/useCosmetics.jsx
+++ src/Hooks/useCosmetics.jsx
@@ -2,16 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const useCosmetics = () => {
-    const { data: cosmetics, isLoading: isCosmeticsLoading, refetch } = useQuery({
-        queryKey: ['cosmetics'],
-        queryFn: async() => {
-            return axios.get('https://e-commerce-server-side-eosin.vercel.app/products?category=cosmetics')
-                .then(result => {
-                    return result.data;
-                })
-        },
-      })
-      return [cosmetics, isCosmeticsLoading, refetch]
+  const {
+    data: cosmetics,
+    isLoading: isCosmeticsLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["cosmetics"],
+    queryFn: async () => {
+      return axios
+        .get(
+          "https://e-commerce-server-side-eosin.vercel.app/products?category=cosmetics"
+        )
+        .then((result) => {
+          return result.data;
+        });
+    },
+  });
+  return [cosmetics, isCosmeticsLoading, refetch];
 };
 
 export default useCosmetics;
